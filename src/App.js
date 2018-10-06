78b058152b05660e798650c037f7c6fea878aa15
David <david.alekna@msknote.com>
2018-10-06 12:53:23 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DataBrowser from 'react-data-browser';
+import axios from 'axios';
 import styles from './App.module.css';
 
 class App extends Component {
